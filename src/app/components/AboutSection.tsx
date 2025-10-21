41a8bf45533ba72c771aef6e786e77b39974dca4
CGE_W11 <CGE_W11@home.dtscrm.cl>
2025-10-21 16:27:26 -0300

    se agrega AboutSection

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -89,9 +89,9 @@ export default function AboutSection() {
 
                     <div className="team-grid" style={{ perspective: 1200 }}>
                         {[
-                            { img: "/team-ux.jpg", name: "Carla", role: "UX/UI" },
-                            { img: "/team-seo.jpg", name: "Leo", role: "SEO & Contenido" },
-                            { img: "/team-dev.jpg", name: "Maca", role: "Front-end" }
+                            { img: "/team-ux.jpg", name: "Javier", role: "UX/UI" },
+                            { img: "/team-seo.jpg", name: "Camilo", role: "SEO & Contenido" },
+                            { img: "/team-dev.jpg", name: "Maximiliano", role: "Front-end" }
                         ].map((m, i) => (
                             <div className="member tilt" key={i} tabIndex={0}>
                                 <div className="avatar small">
